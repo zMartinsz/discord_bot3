9c340bbd2a7a170949a88e69f346acf9fd4e058b
zMartinsz <maugusto22112008@gmail.com>
2025-03-22 14:17:30 -0300

    v2.46

---
+++ bot/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+// Rota de exemplo
+app.get("/", (req, res) => {
+  res.send("Bot rodando...");
+});
+
+// Definindo a porta
+const PORT = process.env.PORT || 3000;
+
+// URL completo do serviço
+const appUrl = `https://${process.env.RENDER_EXTERNAL_URL}`;
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor rodando! Acesse: ${appUrl}`);
+});
