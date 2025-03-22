763e41ff37bacfb6902759d11290eb2d3f859d5e
zMartinsz <maugusto22112008@gmail.com>
2025-03-22 14:36:50 -0300

    v2.6

--- bot/index.js
+++ bot/index.js
@@ -77,3 +77,21 @@ client.on("interactionCreate", async (interaction) => {
   await Promise.all([loadEvents(), loadCommands()]);
   client.login(process.env.TOKEN);
 })();
+
+const express = require("express");
+const app = express();
+
+// Rota de exemplo
+app.get("/", (req, res) => {
+  res.write("Bot rodando...");
+});
+
+// Definindo a porta
+const PORT = process.env.PORT || 3000;
+
+// URL completo do serviço
+const appUrl = `https://${process.env.RENDER_EXTERNAL_URL}`;
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor rodando! Acesse: ${appUrl}`);
+});
