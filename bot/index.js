75a58a98215f19cfd4870660ab8848c427604346
zMartinsz <maugusto22112008@gmail.com>
2025-03-22 19:08:42 -0300

    v2.7

--- bot/index.js
+++ bot/index.js
@@ -2,7 +2,7 @@ const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const fs = require("fs").promises;
 const path = require("path");
 require("dotenv").config();
-
+const keep_alive = require("./keep_alive.js");
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
